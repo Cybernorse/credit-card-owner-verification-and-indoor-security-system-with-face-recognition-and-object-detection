f07e18cb58708c3dee1ab4d2f67696c0138a43bb
WaqarAhmad <42264502+Cybernorse@users.noreply.github.com>
2021-07-02 21:42:12 +0500

    Delete tables.py

--- tables.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("/home/bigpenguin/code/fyp_codes/Accounts.db")
-
-cursor = conn.cursor()
-
-cursor.execute(""" 
-CREATE TABLE account_data (image BLOP,name TEXT,email TEXT,phone TEXT)
-""")
